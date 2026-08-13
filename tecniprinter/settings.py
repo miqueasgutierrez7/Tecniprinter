46ac9f05d1bfd5c0bccf90c6294198f0f7a43ab9
Miqueas <miqueasgutierrez1992@gmail.com>
2026-08-12 22:59:25 -0500

    configurando hora de colombia

--- tecniprinter/settings.py
+++ tecniprinter/settings.py
@@ -108,14 +108,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
-USE_I18N = True
-
+LANGUAGE_CODE = "es-co"   # idioma español Colombia
+TIME_ZONE = "America/Bogota"  # zona horaria Colombia
 USE_TZ = True
 
+USE_I18N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
